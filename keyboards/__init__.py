8880ae837683b550f5a2545ce211d2792affdbb2
SilverXuz <silver-xuz@mail.ru>
2022-10-23 20:57:38 +0300

    go

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,2 +1,6 @@
 from keyboards.client_kb import genMenu
 from keyboards.notebook_kb import notebookMenu
+from keyboards.notebook_kb import cancelButton1
+from keyboards.notebook_kb import cancelButton2
+from keyboards.notebook_kb import cancelButton3
+from keyboards.notebook_kb import cancelButton4
--- keyboards/notebook_kb.py
+++ keyboards/notebook_kb.py
@@ -1,8 +1,7 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
 # Кнопки клавиатуры главного меню
-
-
 btnaddContact = KeyboardButton('Добавить контакт')
 btneditContact = KeyboardButton('Изменить контакт')
 btndelContact = KeyboardButton('Удалить контакт')
@@ -12,3 +11,22 @@ btnBack = KeyboardButton('🔙 Выход')
 
 notebookMenu = ReplyKeyboardMarkup(resize_keyboard=True) 
 notebookMenu.add(btnaddContact, btnoneContact).add(btneditContact, btndelContact).add(btnallContact).add(btnBack)
+
+
+# Меню отмены
+btnCancel1 = KeyboardButton('отмена добавления')
+btnCancel2 = KeyboardButton('отмена изменения')
+btnCancel3 = KeyboardButton('отмена удаления')
+btnCancel4 = KeyboardButton('отмена поиска')
+
+cancelButton1 = ReplyKeyboardMarkup(resize_keyboard=True)
+cancelButton1.add(btnCancel1)
+
+cancelButton2 = ReplyKeyboardMarkup(resize_keyboard=True)
+cancelButton2.add(btnCancel2)
+
+cancelButton3 = ReplyKeyboardMarkup(resize_keyboard=True)
+cancelButton3.add(btnCancel3)
+
+cancelButton4 = ReplyKeyboardMarkup(resize_keyboard=True)
+cancelButton4.add(btnCancel4)
